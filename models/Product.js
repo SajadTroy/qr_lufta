224a81e783cf806548a59266765bda0941dc276e
mrsajadpp <mrsajadpp@gmail.com>
2025-06-09 18:14:30 +0530

    Add Product model and configure Cloudinary for image management

---
+++ models/Product.js
@@ -0,0 +1,10 @@
+// models/Product.js
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  imageUrl: String,
+});
+
+export default mongoose.models.Product || mongoose.model("Product", ProductSchema);
