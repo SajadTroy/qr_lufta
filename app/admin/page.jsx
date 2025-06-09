6bfd3e4851f48867a332c7e09a67645213eb08de
mrsajadpp <mrsajadpp@gmail.com>
2025-06-09 19:42:35 +0530

    Implement QR code scanning functionality in admin page and add html5-qrcode dependency

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,11 +1,41 @@
+'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Html5QrcodeScanner } from "html5-qrcode";
+import { useEffect, useState } from "react";
 
 export default function AdminHome() {
-    return (
-        <div className={styles.page}>
-            <main className={styles.main}>
-            </main>
-        </div>
-    );
+
+  const [scanResult, setScanResult] = useState(null);
+
+  useEffect(() => {
+    const scanner = new Html5QrcodeScanner('reader', {
+      qrbox: {
+        width: 250,
+        height: 250,
+      },
+      fps: 5,
+    });
+
+    scanner.render(success, error);
+
+    function success(result) {
+      scanner.clear();
+      setScanResult(result)
+    }
+
+    function error(err) {
+      console.warn(err);
+    }
+  }, []);
+
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        {
+          scanResult ? (<p>{scanResult}</p>) : (<div id="reader"></div>)
+        }
+      </main>
+    </div>
+  );
 }
